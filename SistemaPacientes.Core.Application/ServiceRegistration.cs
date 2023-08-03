1762943d52548b564cf248a0ac37f6a2ed47e8fe
Addiel <addieljaquez@gmail.com>
2023-08-03 16:56:27 -0400

    InitialCreating

---
+++ SistemaPacientes.Core.Application/ServiceRegistration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPacientes.Core.Application
+{
+    public class ServiceRegistration
+    {
+
+    }
+}
